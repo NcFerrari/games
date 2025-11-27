4cf54e552fe9ca3735e35f0e68dca7f7d6020510
lubor.pesek <lubor.pesek@qinshift.com>
2025-11-27 15:40:55 +0100

    working on production actions

--- Settlers/frontend/src/main/java/cz/games/lp/actions/ProductionActions.java
+++ Settlers/frontend/src/main/java/cz/games/lp/actions/ProductionActions.java
@@ -1,7 +1,6 @@
 package cz.games.lp.actions;
 
 import cz.games.lp.components.Card;
-import cz.games.lp.components.SourceStatusBlock;
 import cz.games.lp.enums.CardType;
 import cz.games.lp.enums.Sources;
 import cz.games.lp.panes.PaneModel;
@@ -9,8 +8,6 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.util.Duration;
 
-import java.util.Map;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class ProductionActions {
@@ -25,21 +22,23 @@ public class ProductionActions {
     }
 
     public void proceedProduction(Runnable switchPhase) {
-        if (isTimeLineRunning) {
-            return;
-        }
-        timeline.getKeyFrames().clear();
-        atomicIndex.set(0);
-        produceFactionCards();
-        produceDeals();
-        produceFactionBoard();
-        produceCommonCards();
-        timeline.setOnFinished(evt -> {
-//            switchPhase.run();
-            isTimeLineRunning = false;
-        });
-        isTimeLineRunning = true;
-        timeline.play();
+
+//        if (isTimeLineRunning) {
+//            return;
+//        }
+//
+//        timeline.getKeyFrames().clear();
+//        atomicIndex.set(0);
+//        produceFactionCards();
+//        produceDeals();
+//        produceFactionBoard();
+//        produceCommonCards();
+//        timeline.setOnFinished(evt -> {
+////            switchPhase.run();
+//            isTimeLineRunning = false;
+//        });
+//        isTimeLineRunning = true;
+//        timeline.play();
     }
 
     private void produceFactionCards() {
@@ -63,11 +62,6 @@ public class ProductionActions {
     }
 
     private void produceCommonCards() {
-//        model.getBuiltCommonCards().get(CardType.PRODUCTION).getChildren().forEach(card -> timeline.getKeyFrames().addAll(
-//                new KeyFrame(Duration.seconds(atomicIndex.get()), e -> ((Card) card).select()),
-//                new KeyFrame(Duration.seconds(atomicIndex.getAndIncrement()), e -> executeEffect((Card) card)),
-//                new KeyFrame(Duration.seconds(atomicIndex.get()), e -> ((Card) card).deselect())
-//        ));
     }
 
     private void executeCardEffect(Card card) {
@@ -81,29 +75,5 @@ public class ProductionActions {
 
     private void getDeal(Card card) {
         model.getSources().get(Sources.valueOf(card.getDealSource())).addOne();
-    }
-
-    public CompletableFuture<Void> produce(Card card, Map<Sources, SourceStatusBlock> sources) {
-//        if (card.getCardEffect().contains("|||")) {
-//            String[] choices = card.getCardEffect().split("\\|\\|");
-//            CompletableFuture[] futures = new CompletableFuture[choices.length];
-//            for (int i = 0; i < choices.length; i++) {
-//                futures[i] = sources.get(Sources.valueOf(choices[i])).waitForSelection();
-//            }
-//            return CompletableFuture.anyOf(futures).thenAccept(selectedObj -> {
-//                SourceStatusBlock selected = (SourceStatusBlock) selectedObj;
-//                selected.addOne();
-//                for (String choice : choices) {
-//                    sources.get(Sources.valueOf(choice)).deselect();
-//                }
-//            }).thenApply(v -> null);
-//        }
-//        if (card.getCardEffect().contains("&&")) {
-//            Arrays.stream(card.getCardEffect().split("&&")).forEach(source ->
-//                    sources.get(Sources.valueOf(source)).addOne()
-//            );
-//        }
-//        return CompletableFuture.completedFuture(null);
-        return null;
     }
 }
