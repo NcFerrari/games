4f66fd8f1375cd7384c278ef26da6175f436b04b
Lubor <lubor.pesek@post.cz>
2025-12-09 07:52:11 +0100

    prepared for japan production (at first - needed to refactor)

--- Settlers/frontend/src/main/java/cz/games/lp/frontend/actions/ProductionActions.java
+++ Settlers/frontend/src/main/java/cz/games/lp/frontend/actions/ProductionActions.java
@@ -4,6 +4,7 @@ import cz.games.lp.common.enums.CardTypes;
 import cz.games.lp.common.enums.Sources;
 import cz.games.lp.frontend.components.ImageNode;
 import cz.games.lp.frontend.components.transition_components.Card;
+import cz.games.lp.frontend.enums.ProductionBlocks;
 import cz.games.lp.frontend.models.CommonModel;
 import javafx.animation.FadeTransition;
 import javafx.animation.ParallelTransition;
@@ -28,6 +29,7 @@ public class ProductionActions {
     private final AtomicLong stopTime = new AtomicLong();
     private final AtomicInteger counter = new AtomicInteger();
     private Card selectedCard;
+    private ProductionBlocks block;
 
     public ProductionActions(CommonModel model) {
         this.model = model;
@@ -35,64 +37,94 @@ public class ProductionActions {
     }
 
     public Consumer<Long> proceedProduction(ActionManager actionManager) {
-        ObservableList<Node> factionCards = ((HBox) model.getFactionCards().get(CardTypes.PRODUCTION).getContent()).getChildren();
-        ObservableList<Node> deals = model.getDeals().getDeals();
-        List<Sources> factionBoardSources = model.getFactionBoard().getFactionData().getFactionProduction();
-        ObservableList<Node> commonCards = ((HBox) model.getCommonCards().get(CardTypes.PRODUCTION).getContent()).getChildren();
-
-        counter.set(factionCards.size());
+        block = ProductionBlocks.FACTIONS;
         return time -> {
-            if (time - stopTime.get() < DELAY * 1_000_000) {
-                return;
+            switch (block) {
+                case ProductionBlocks.FACTIONS -> factions();
+                case ProductionBlocks.DEALS -> deals();
+                case ProductionBlocks.FACTION_BOARD -> factionBoard();
+                case ProductionBlocks.COMMONS -> commons();
+                default -> actionManager.stop();
             }
-            selectedCard.deselect();
-            if (counter.get() == 0) {
-                actionManager.stop();
-                actionManager.setAnimationRunning(false);
-                return;
-            }
-            stopTime.set(time);
-
-            double hValue = 1.0 / ((HBox) model.getFactionCards().get(CardTypes.PRODUCTION).getContent()).getChildren().size();
-            model.getFactionCards().get(CardTypes.PRODUCTION).setHvalue(1 - hValue * (counter.get() - 1));
-
-            selectedCard = (Card) ((HBox) model.getFactionCards().get(CardTypes.PRODUCTION).getContent()).getChildren().get(counter.decrementAndGet());
-            selectedCard.select();
-            SequentialTransition sequentialTransition = new SequentialTransition();
-
-            model.getManager().getCard(selectedCard.getCardId()).getCardEffect().forEach(effect -> {
-                ImageNode imageNode = new ImageNode(30, 30);
-                imageNode.setImage("source/" + effect.name().toLowerCase());
-                imageNode.getImageView().setX(model.getUIConfig().getCardWidth() / 2 - imageNode.getImageView().getFitWidth() / 2);
-                imageNode.getImageView().setY(3 * model.getUIConfig().getCardHeight() / 4 - imageNode.getImageView().getFitHeight() / 2);
-                imageNode.getImageView().setOpacity(0);
-                selectedCard.getChildren().add(imageNode.getImageView());
-
-                Transition transition = new Transition() {
-                    @Override
-                    protected void interpolate(double v) {
-                        imageNode.getImageView().setOpacity(100);
-                        model.getOwnSupplies().get(Sources.valueOf(effect.name())).addOne();
-                    }
-                };
-
-                TranslateTransition translateTransition = new TranslateTransition(Duration.millis(DELAY / 2), imageNode.getImageView());
-                translateTransition.setToY(-60);
-
-                FadeTransition fadeTransition = new FadeTransition(Duration.millis(DELAY / 2), imageNode.getImageView());
-                fadeTransition.setToValue(0);
-
-                ScaleTransition scaleTransition = new ScaleTransition(Duration.millis(DELAY / 2), imageNode.getImageView());
-                scaleTransition.setToX(2);
-                scaleTransition.setToY(2);
-
-                ParallelTransition parallelTransition = new ParallelTransition();
-                parallelTransition.getChildren().addAll(transition, translateTransition, fadeTransition, scaleTransition);
-                parallelTransition.setOnFinished(e -> selectedCard.getChildren().remove(imageNode.getImageView()));
-
-                sequentialTransition.getChildren().add(parallelTransition);
-            });
-            sequentialTransition.play();
         };
+
+//        counter.set(factionCards.size());
+//        return time -> {
+//            if (time - stopTime.get() < DELAY * 1_000_000) {
+//                return;
+//            }
+//            selectedCard.deselect();
+//            if (counter.get() == 0) {
+//                actionManager.stop();
+//                actionManager.setAnimationRunning(false);
+//                return;
+//            }
+//            stopTime.set(time);
+//
+//            double hValue = 1.0 / ((HBox) model.getFactionCards().get(CardTypes.PRODUCTION).getContent()).getChildren().size();
+//            model.getFactionCards().get(CardTypes.PRODUCTION).setHvalue(1 - hValue * (counter.get() - 1));
+//
+//            selectedCard = (Card) ((HBox) model.getFactionCards().get(CardTypes.PRODUCTION).getContent()).getChildren().get(counter.decrementAndGet());
+//            selectedCard.select();
+//            SequentialTransition sequentialTransition = new SequentialTransition();
+//
+//            model.getManager().getCard(selectedCard.getCardId()).getCardEffect().forEach(effect -> {
+//                ImageNode imageNode = new ImageNode(30, 30);
+//                imageNode.setImage("source/" + effect.name().toLowerCase());
+//                imageNode.getImageView().setX(model.getUIConfig().getCardWidth() / 2 - imageNode.getImageView().getFitWidth() / 2);
+//                imageNode.getImageView().setY(3 * model.getUIConfig().getCardHeight() / 4 - imageNode.getImageView().getFitHeight() / 2);
+//                imageNode.getImageView().setOpacity(0);
+//                selectedCard.getChildren().add(imageNode.getImageView());
+//
+//                Transition transition = new Transition() {
+//                    @Override
+//                    protected void interpolate(double v) {
+//                        imageNode.getImageView().setOpacity(100);
+//                        model.getOwnSupplies().get(Sources.valueOf(effect.name())).addOne();
+//                    }
+//                };
+//
+//                TranslateTransition translateTransition = new TranslateTransition(Duration.millis(DELAY / 2), imageNode.getImageView());
+//                translateTransition.setToY(-60);
+//
+//                FadeTransition fadeTransition = new FadeTransition(Duration.millis(DELAY / 2), imageNode.getImageView());
+//                fadeTransition.setToValue(0);
+//
+//                ScaleTransition scaleTransition = new ScaleTransition(Duration.millis(DELAY / 2), imageNode.getImageView());
+//                scaleTransition.setToX(2);
+//                scaleTransition.setToY(2);
+//
+//                ParallelTransition parallelTransition = new ParallelTransition();
+//                parallelTransition.getChildren().addAll(transition, translateTransition, fadeTransition, scaleTransition);
+//                parallelTransition.setOnFinished(e -> selectedCard.getChildren().remove(imageNode.getImageView()));
+//
+//                sequentialTransition.getChildren().add(parallelTransition);
+//            });
+//            sequentialTransition.play();
+//        };
+    }
+
+    private void factions() {
+        ObservableList<Node> factionCards = ((HBox) model.getFactionCards().get(CardTypes.PRODUCTION).getContent()).getChildren();
+        System.out.println("1");
+        block = ProductionBlocks.DEALS;
+    }
+
+    private void deals() {
+        ObservableList<Node> deals = model.getDeals().getDeals();
+        System.out.println("2");
+        block = ProductionBlocks.FACTION_BOARD;
+    }
+
+    private void factionBoard() {
+        List<Sources> factionBoardSources = model.getFactionBoard().getFactionData().getFactionProduction();
+        System.out.println("3");
+        block = ProductionBlocks.COMMONS;
+    }
+
+    private void commons() {
+        ObservableList<Node> commonCards = ((HBox) model.getCommonCards().get(CardTypes.PRODUCTION).getContent()).getChildren();
+        System.out.println("4");
+        block = ProductionBlocks.DEFAULT;
     }
 }
